7fa0c2cec2b6099ddeef24f27ff20a35ec0dc5d5
Marcos Nascimento <marcozzymandias@gmail.com>
2021-02-01 16:01:04 -0300

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -40,9 +40,24 @@ export default function Home() {
     <QuizBackground backgroundImage={db.bg}>
       <Head>
         <title>
-          AluraQuiz -
+          AluraQuiz - HTML - THE BEST PROGRAMMING LANGUAGE
           {db.title}
         </title>
+
+        <meta name="title" content="HTML - THE BEST PROGRAMMING LANGUAGE" />
+        <meta name="description" content="Projeto de Next.js realizado durante a Imersão React v2 da Alura!" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="HTML - THE BEST PROGRAMMING LANGUAGE" />
+        <meta property="og:description" content="Projeto de Next.js realizado durante a Imersão React v2 da Alura!" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="HTML - THE BEST PROGRAMMING LANGUAGE" />
+        <meta property="twitter:description" content="Projeto de Next.js realizado durante a Imersão React v2 da Alura!" />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       </Head>
       <QuizContainer>
         <QuizLogo />
